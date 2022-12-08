30164e532c9c4a9507a17188a91f84344c802e48
BambooTuna <bambootuna@gmail.com>
2022-12-09 03:38:29 +0900

    add setup script

--- dotfiles/.bin/link.sh
+++ dotfiles/.bin/link.sh
@@ -12,6 +12,12 @@ for dotfile in `\find ${SCRIPT_DIR} -name '.??*' -type f`; do
     ln -fnsv "$dotfile" "$HOME"
 done
 
+for dotfile in `\find ${SCRIPT_DIR}/.config -type f`; do
+    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+    mkdir -p $(dirname $dotfile)
+    ln -fnsv "$dotfile" "$HOME"
+done
+
 # for dotfile in "${SCRIPT_DIR}"/.??* ; do
 #     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
 #     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
