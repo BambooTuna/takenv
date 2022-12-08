75facbaa80660c0918ad98d6aac35dc3cdd4cd00
BambooTuna <bambootuna@gmail.com>
2022-12-09 03:52:09 +0900

    add setup script

--- dotfiles/.bin/link.sh
+++ dotfiles/.bin/link.sh
@@ -7,28 +7,18 @@ realpath() {
 }
 
 for dotfile in `\find . -name '.??*' -type f`; do
-    [[ "$dotfile" == ".bin" ]] && continue
-    [[ "$dotfile" == ".git" ]] && continue
-    [[ "$dotfile" == ".github" ]] && continue
     [[ "$dotfile" == ".DS_Store" ]] && continue
 
     echo "$(realpath $dotfile) -> $HOME/$(dirname $dotfile)"
-    # ln -fnsv "$(realpath $dotfile)" "$HOME/$(dirname $dotfile)"
+    mkdir -p $(dirname $dotfile)
+    ln -fnsv "$(realpath $dotfile)" "$HOME/$(dirname $dotfile)"
 done
 
 for dotfile in `\find .config -type f`; do
     [[ "$dotfile" == ".DS_Store" ]] && continue
 
     echo "$(realpath $dotfile) -> $HOME/$(dirname $dotfile)"
-
     mkdir -p $(dirname $dotfile)
-    # ln -fnsv "$dotfile" "$HOME"
+    ln -fnsv "$(realpath $dotfile)" "$HOME/$(dirname $dotfile)"
 done
 
-# for dotfile in "${SCRIPT_DIR}"/.??* ; do
-#     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
-#     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
-#     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
-
-#     ln -fnsv "$dotfile" "$HOME"
-# done
