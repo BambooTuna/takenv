26479efa2e5974d12f53453a6854bf4d5da9bf9e
BambooTuna <bambootuna@gmail.com>
2022-12-09 03:45:45 +0900

    add setup script

--- dotfiles/.bin/link.sh
+++ dotfiles/.bin/link.sh
@@ -2,20 +2,27 @@
 
 SCRIPT_DIR="$(pwd)"
 
-for dotfile in `\find ${SCRIPT_DIR} -name '.??*' -type f`; do
-    [[ "$dotfile" == "${SCRIPT_DIR}/.bin" ]] && continue
-    [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
-    [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
-    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+realpath() {
+  case "$1" in /*) ;; *) printf '%s/' "$PWD";; esac; echo "$1"
+}
 
-    mkdir -p $(dirname $dotfile)
-    ln -fnsv "$dotfile" "$HOME"
+for dotfile in `\find . -name '.??*' -type f`; do
+    [[ "$dotfile" == ".bin" ]] && continue
+    [[ "$dotfile" == ".git" ]] && continue
+    [[ "$dotfile" == ".github" ]] && continue
+    [[ "$dotfile" == ".DS_Store" ]] && continue
+
+    echo "$(realpath $dotfile) -> $HOME/$(dirname $dotfile)"
+    # ln -fnsv "$(realpath $dotfile)" "$HOME/$(dirname $dotfile)"
 done
 
-for dotfile in `\find ${SCRIPT_DIR}/.config -type f`; do
-    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+for dotfile in `\find .config -type f`; do
+    [[ "$dotfile" == ".DS_Store" ]] && continue
+
+    echo "$(realpath $dotfile) -> $HOME/$(dirname $dotfile)"
+
     mkdir -p $(dirname $dotfile)
-    ln -fnsv "$dotfile" "$HOME"
+    # ln -fnsv "$dotfile" "$HOME"
 done
 
 # for dotfile in "${SCRIPT_DIR}"/.??* ; do
