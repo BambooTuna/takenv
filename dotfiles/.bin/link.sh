a36289608d7f49ebfb6c5efde4680c6150795a0c
BambooTuna <bambootuna@gmail.com>
2022-12-09 03:35:50 +0900

    add setup script

--- dotfiles/.bin/link.sh
+++ dotfiles/.bin/link.sh
@@ -2,10 +2,20 @@
 
 SCRIPT_DIR="$(pwd)"
 
-for dotfile in "${SCRIPT_DIR}"/.??* ; do
+for dotfile in `\find ${SCRIPT_DIR} -name '.??*' -type f`; do
+    [[ "$dotfile" == "${SCRIPT_DIR}/.bin" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
 
+    mkdir -p $(dirname $dotfile)
     ln -fnsv "$dotfile" "$HOME"
 done
+
+# for dotfile in "${SCRIPT_DIR}"/.??* ; do
+#     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+#     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
+#     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+
+#     ln -fnsv "$dotfile" "$HOME"
+# done
