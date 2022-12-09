400dc61e1fb5069354e88e49e62a8831ada1188c
BambooTuna <bambootuna@gmail.com>
2022-12-09 12:09:20 +0900

    fzf setting

--- dotfiles/.bin/setup.sh
+++ dotfiles/.bin/setup.sh
@@ -3,13 +3,11 @@
 if [ "$(uname)" == "Darwin" ] ; then
   brew bundle --global
   zsh "$(dirname $0)/setup-asdf-plugin.zsh" nodejs latest
-  zsh "$(dirname $0)/setup-asdf-plugin.zsh" fzf latest
   zsh "$(dirname $0)/setup-asdf-plugin.zsh" neovim latest
 elif [ "$(uname)" == "Linux" ] ; then
   # install asdf
   git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.10.2
   bash "$(dirname $0)/setup-asdf-plugin.bash" nodejs latest
-  bash "$(dirname $0)/setup-asdf-plugin.bash" fzf latest
 
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
