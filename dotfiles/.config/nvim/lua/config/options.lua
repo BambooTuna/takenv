d12720b211638f8a1d23b54f7667647589bd2ff0
Suzuki Takeo <bambootuna@gmail.com>
2025-10-21 10:07:44 +0000

    clipboard

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -3,3 +3,19 @@
 -- Add any additional options here
 
 vim.g.snacks_animate = false
+
+-- クリップボード設定（SSH/Docker越しでも動作するようOSC52を使用）
+vim.opt.clipboard = "unnamedplus"
+
+-- OSC52を明示的に設定（リモート環境でのクリップボード同期）
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
