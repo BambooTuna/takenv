09dd6804ab9507e407f82c200e372f4bfe401865
BambooTuna <bambootuna@gmail.com>
2026-07-24 20:09:42 +0900

    feat: WezTerm に Cmd+1..9 のタブ切替キーバインドを追加

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -58,6 +58,16 @@ return {
 		-- Cmd+[ / ]: スペース内のタブ移動
 		{ key = "[", mods = "SUPER", action = herdr({ key = "p" }) },
 		{ key = "]", mods = "SUPER", action = herdr({ key = "n" }) },
+		-- Cmd+1..9: wezterm 純正のタブ切替（herdr のタブ移動は Cmd+[ / ] を使う）
+		{ key = "1", mods = "SUPER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "SUPER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "SUPER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "SUPER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "SUPER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "SUPER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "SUPER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "SUPER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "SUPER", action = wezterm.action({ ActivateTab = 8 }) },
 		-- Cmd+Shift+[ / ]: スペース（プロジェクト）間の移動
 		-- macOSのキー解釈が環境依存のため、届きうる表現をすべて登録する
 		{ key = "[", mods = "SHIFT|SUPER", action = herdr({ key = "UpArrow" }) },
